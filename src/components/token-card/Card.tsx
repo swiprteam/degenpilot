617d11c60abd63a2b37a7b28873385e392d5b7ce
the-big-rabbit <69635920+the-big-rabbit@users.noreply.github.com>
2024-10-13 14:41:36 +0200

    info button

--- src/components/token-card/Card.tsx
+++ src/components/token-card/Card.tsx
@@ -6,24 +6,23 @@ import Score from "./Score";
 import Swap from "./Swap";
 
 type Props = {
-  token: TokenInterface;
+    token: TokenInterface;
 };
 
 const TokenCard = ({ token }: Props) => {
-  return (
-    <StyledTokenCard className="mt-4 flex flex-col overflow-hidden">
-      <Score token={token} />
-      <Header token={token} />
-      <Swap token={token} />
-    </StyledTokenCard>
-  );
+    return (
+        <StyledTokenCard className="mt-4 flex flex-col overflow-hidden">
+            <Score token={token} />
+            {/* <Swap token={token} /> */}
+        </StyledTokenCard>
+    );
 };
 
 const StyledTokenCard = styled.div`
-  touch-action: pan-y; /* Allows vertical scrolling */
-  background: #282828;
-  border-radius: 20px;
-  padding: 20px;
+    touch-action: pan-y; /* Allows vertical scrolling */
+    background: #282828;
+    border-radius: 20px;
+    padding: 20px;
 `;
 
 export default TokenCard;
