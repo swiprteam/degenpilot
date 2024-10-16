7b600796005c59c9e922fb369fd4b97d5d8e1e0b
NoOne <noone@no-one.com>
2024-10-16 22:05:54 +0200

    Fix slider

--- src/components/TokenList.tsx
+++ src/components/TokenList.tsx
@@ -9,130 +9,158 @@ import Up from "/up.png";
 import Down from "/down.png";
 import { useRef, useState } from "react";
 import { useMemo } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
 import { Keyboard } from "swiper/modules";
 import { next, prev } from "~/services/tokens";
 import Buy from "./token-card/Buy";
 
 const TokenList = () => {
-    const tokens = useTokens();
-    const selectedToken = useSelectedToken();
-    const swiperRef = useRef(null);
-    const [isAnimating, setIsAnimating] = useState(false);
-    const initialIndex = useMemo(() => {
-        if (selectedToken.index < 1) return selectedToken.index;
-        return 1;
-    }, [selectedToken.index]);
+  const tokens = useTokens();
 
-    const handlePrevClick = () => {
-        if (swiperRef.current && !isAnimating) {
-            setIsAnimating(true);
-            swiperRef.current.slidePrev();
-            setTimeout(() => {
-                prev();
-                setIsAnimating(false);
-            }, 600);
-        }
-    };
+  const selectedToken = useSelectedToken();
+  const swiperRef = useRef<SwiperClass>(null);
 
-    const handleNextClick = () => {
-        if (swiperRef.current && !isAnimating) {
-            setIsAnimating(true);
-            swiperRef.current.slideNext();
-            setTimeout(() => {
-                next();
-                setIsAnimating(false);
-            }, 600);
-        }
-    };
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isInit, setIsInit] = useState(false);
 
-    return (
-        <AppLayout>
-            <AuthLayout>
-                <div className="wrapperScreen mb-8 md:p-8">
-                    <Header />
-                    <Swiper
-                        onSwiper={(swiper) => (swiperRef.current = swiper)}
-                        modules={[Keyboard]}
-                        key={selectedToken.index}
-                        direction="vertical"
-                        className="swipToken"
-                        initialSlide={initialIndex}
-                        keyboard={{
-                            enabled: true,
-                            onlyInViewport: false,
-                        }}
-                        speed={500}
-                        effect="slide"
-                        slidesPerView={1.2}
-                        centeredSlides={true}
-                        spaceBetween={30}
-                        onSlideChangeTransitionEnd={(e) => {
-                            if (e.swipeDirection === "prev") prev();
-                            else if (e.swipeDirection === "next") next();
-                        }}>
-                        {tokens.map((token, _index) => {
-                            const isPrev =
-                                token.index < selectedToken.index &&
-                                token.index >= selectedToken.index - 1;
+  const initialIndex = useMemo(() => {
+    if (selectedToken.index < 2) return selectedToken.index;
+    return 2;
+  }, [selectedToken.index]);
 
-                            const isNext =
-                                token.index > selectedToken.index &&
-                                token.index <= selectedToken.index + 1;
-                            const isCurrent =
-                                selectedToken.index === token.index;
-                            if (!isCurrent && !isNext && !isPrev) return;
+  const handlePrevClick = () => {
+    if (swiperRef.current && !isAnimating) {
+      setIsAnimating(true);
+      swiperRef.current.slidePrev();
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 600);
+    }
+  };
 
-                            return (
-                                <>
-                                    <SwiperSlide
-                                        key={token.id}
-                                        data-id={token.id}
-                                        className={clsx(
-                                            `token-${token.index} token-${token.info.symbol}`,
-                                            {
-                                                selected: isCurrent,
-                                                prev: isPrev,
-                                                next: isNext,
-                                            }
-                                        )}>
-                                        <TokenCard token={token} />
-                                    </SwiperSlide>
-                                    {_index === tokens.length - 1 && (
-                                        <SwiperSlide
-                                            key={tokens[0].id}
-                                            data-id={tokens[0].id}
-                                            data-symbol={tokens[0].info.symbol}
-                                            className={clsx(
-                                                `token-${tokens[0].index}`,
-                                                {
-                                                    selected: false,
-                                                    prev: false,
-                                                    next: true,
-                                                }
-                                            )}>
-                                            <TokenCard token={tokens[0]} />
-                                        </SwiperSlide>
-                                    )}
-                                </>
-                            );
-                        })}
-                    </Swiper>
-                    <Buy />
-                    <div
-                        onClick={handlePrevClick}
-                        className="nav-swiper swiper-button-prev">
-                        <img src={Up} alt="up" />
-                    </div>
-                    <div
-                        onClick={handleNextClick}
-                        className="nav-swiper swiper-button-next">
-                        <img src={Down} alt="down" />
-                    </div>
-                </div>
-            </AuthLayout>
-        </AppLayout>
-    );
+  const handleNextClick = () => {
+    if (swiperRef.current && !isAnimating) {
+      setIsAnimating(true);
+      swiperRef.current.slideNext();
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 600);
+    }
+  };
+
+  const visibleTokens = useMemo(() => {
+    return tokens.filter((token) => {
+      const isPrev =
+        token.index < selectedToken.index &&
+        token.index >= selectedToken.index - 2;
+
+      const isNext =
+        token.index > selectedToken.index &&
+        token.index <= selectedToken.index + 2;
+      const isCurrent = selectedToken.index === token.index;
+      return isCurrent || isNext || isPrev;
+    });
+  }, [selectedToken.index, tokens]);
+
+  return (
+    <AppLayout>
+      <AuthLayout>
+        <div className="wrapperScreen mb-8 md:p-8">
+          <Header />
+          <Swiper
+            onSwiper={(swiper) => {
+              swiperRef.current = swiper;
+              setIsInit(true);
+            }}
+            modules={[Keyboard]}
+            direction="vertical"
+            className="swipToken"
+            initialSlide={initialIndex}
+            keyboard={{
+              enabled: true,
+              onlyInViewport: false,
+            }}
+            speed={500}
+            effect="slide"
+            slidesPerView={1.2}
+            centeredSlides={true}
+            spaceBetween={30}
+            onSlidePrevTransitionEnd={() => {
+              if (!isInit) return;
+              prev();
+
+              if (selectedToken.index > 2) {
+                swiperRef.current.activeIndex = 2;
+              }
+            }}
+            onSlideNextTransitionEnd={() => {
+              if (!isInit) return;
+              next();
+              if (selectedToken.index > 2) swiperRef.current.activeIndex = 2;
+            }}
+          >
+            {visibleTokens.map((token, _index) => {
+              const isPrev =
+                token.index < selectedToken.index &&
+                token.index >= selectedToken.index - 2;
+
+              const isNext =
+                token.index > selectedToken.index &&
+                token.index <= selectedToken.index + 2;
+              const isCurrent = selectedToken.index === token.index;
+              if (!isCurrent && !isNext && !isPrev) return;
+
+              return (
+                <>
+                  <SwiperSlide
+                    key={token.id}
+                    data-id={token.id}
+                    className={clsx(
+                      `token-${token.index} token-${token.info.symbol}`,
+                      {
+                        selected: isCurrent,
+                        prev: isPrev,
+                        next: isNext,
+                      }
+                    )}
+                  >
+                    <TokenCard token={token} />
+                  </SwiperSlide>
+                  {_index === tokens.length - 1 && (
+                    <SwiperSlide
+                      key={tokens[0].id}
+                      data-id={tokens[0].id}
+                      data-symbol={tokens[0].info.symbol}
+                      className={clsx(`token-${tokens[0].index}`, {
+                        selected: false,
+                        prev: false,
+                        next: true,
+                      })}
+                    >
+                      <TokenCard token={tokens[0]} />
+                    </SwiperSlide>
+                  )}
+                </>
+              );
+            })}
+          </Swiper>
+          <Buy />
+          <div
+            onClick={handlePrevClick}
+            className="nav-swiper swiper-button-prev"
+          >
+            <img src={Up} alt="up" />
+          </div>
+          <div
+            onClick={handleNextClick}
+            className="nav-swiper swiper-button-next"
+          >
+            <img src={Down} alt="down" />
+          </div>
+        </div>
+      </AuthLayout>
+    </AppLayout>
+  );
 };
 
 export default TokenList;
