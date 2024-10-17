e610bdaafa7963ead5f466b714d217b8ea505906
the-big-rabbit <69635920+the-big-rabbit@users.noreply.github.com>
2024-10-17 20:39:20 +0200

    font size button start connect

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,6 +1,6 @@
 import {
-  useWeb3ModalAccount,
-  useWeb3ModalProvider,
+    useWeb3ModalAccount,
+    useWeb3ModalProvider,
 } from "@web3modal/solana/react";
 
 import { ReactNode, useEffect, useState } from "react";
@@ -10,49 +10,48 @@ import styled from "styled-components";
 import Logo from "@/components/Logo";
 
 const AuthLayoutContent = ({ children }: { children: ReactNode }) => {
-  const { isConnected } = useWeb3ModalAccount();
-  const { instance } = useWeb3Modal();
+    const { isConnected } = useWeb3ModalAccount();
+    const { instance } = useWeb3Modal();
 
-  if (!isConnected) {
-    return (
-      <AppLayout>
-        <div className="flex flex-col">
-          <div className="content justify-center flex flex-col wrapper">
-            <h1 className="brandTitle">Swipr</h1>
-            <div className="landingLogoConnect">
-              <Logo />
-              <div className="emphaz">
-                Connect to app.
-                <div className="descLanding">
-                  Connect your wallet
-                  <br /> to Swipr below.
+    if (!isConnected) {
+        return (
+            <AppLayout>
+                <div className="flex flex-col">
+                    <div className="content justify-center flex flex-col wrapper">
+                        <h1 className="brandTitle">Swipr</h1>
+                        <div className="landingLogoConnect">
+                            <Logo />
+                            <div className="emphaz">
+                                Connect to app.
+                                <div className="descLanding">
+                                    Connect your wallet
+                                    <br /> to Swipr below.
+                                </div>
+                            </div>
+                            <Button
+                                className="w-full flex justify-center items-center startConnect"
+                                onClick={() => instance.open()}>
+                                Connect wallet{" "}
+                            </Button>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <Button
-                className="w-full flex justify-center items-center"
-                onClick={() => instance.open()}
-              >
-                Connect wallet{" "}
-              </Button>
-            </div>
-          </div>
-        </div>
-      </AppLayout>
-    );
-  } else {
-    instance.close();
-  }
-  return children;
+            </AppLayout>
+        );
+    } else {
+        instance.close();
+    }
+    return children;
 };
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  const { web3Modal } = useLoading();
+    const { web3Modal } = useLoading();
 
-  if (web3Modal) return;
-  return (
-    <AuthLayoutContent>
-      <>{children}</>
-    </AuthLayoutContent>
-  );
+    if (web3Modal) return;
+    return (
+        <AuthLayoutContent>
+            <>{children}</>
+        </AuthLayoutContent>
+    );
 };
 export default AuthLayout;
